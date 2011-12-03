4ecebc5de9911a4637a8335573bd5e05c409ff25
tehoLabs <babryce@breadbox.(none)>
2011-12-02 22:38:40 -0500

    remove temp files

--- usb_dev_mouse/gcc/usb_mouse_structs.d
+++
@@ -1,9 +0,0 @@
-gcc/usb_mouse_structs.o: usb_mouse_structs.c \
- /home/babryce/Development/StellarisWare/inc/hw_types.h \
- /home/babryce/Development/StellarisWare/usblib/usblib.h \
- /home/babryce/Development/StellarisWare/usblib/usbhid.h \
- /home/babryce/Development/StellarisWare/usblib/usb-ids.h \
- /home/babryce/Development/StellarisWare/usblib/device/usbdevice.h \
- /home/babryce/Development/StellarisWare/usblib/device/usbdhid.h \
- /home/babryce/Development/StellarisWare/usblib/device/usbdhidmouse.h \
- usb_mouse_structs.h
